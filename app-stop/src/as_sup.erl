198d76001d3feefd5272880415ced69d33be1832
Richard Jonas <richard.jonas@wallapop.com>
2017-03-14 17:16:27 +0100

    Fix warnings

--- app-stop/src/as_sup.erl
+++ app-stop/src/as_sup.erl
@@ -1,18 +1,18 @@
 -module(as_sup).
 -behaviour(supervisor).
 
--export([start/0,
+-export([start_link/0,
          init/1]).
 
-start() ->
+start_link() ->
     supervisor:start_link({local, as_sup}, as_sup, []).
 
 init(_Args) ->
     {ok, {#{startegy => one_for_one,
             intensity => 5,
             period => 1000},
-          [child(as_service, []),
-           start_cowboy()
+          [child(as_service, [])
+           %start_cowboy()
           ]}
          }.
 
@@ -24,7 +24,7 @@ child(Module, Args) ->
       type => worker,
       modules => []}.
 
-child_sup(Module, Args) ->
-    Spec = child(Module, Args),
-    Spec#{shutdown => infinity,
-          type => supervisor}.
+%child_sup(Module, Args) ->
+%    Spec = child(Module, Args),
+%    Spec#{shutdown => infinity,
+%          type => supervisor}.
